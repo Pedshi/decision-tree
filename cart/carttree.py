cb8b3556d4597b18df6e762e7d0b8332b3149595
Pedram Shirmohammad <Pedram@pamind.se>
2022-03-20 20:55:04 +0100

    add typing to args

--- cart/carttree.py
+++ cart/carttree.py
@@ -15,14 +15,14 @@ class SplitWithInfo(TypedDict):
 
 class CARTTree:
 
-    def __init__(self, max_depth=None):
+    def __init__(self, max_depth: int = None):
         self.max_depth = max_depth
         self.root = None
 
-    def train(self, data, target):
+    def train(self, data: pd.DataFrame, target: pd.Series):
         self.root = self._train_tree(data, target, 0)
 
-    def _train_tree(self, data, target, depth):
+    def _train_tree(self, data: pd.DataFrame, target: pd.Series, depth: int):
         """Recursively create subtrees that maximizes purity of target """
 
         if is_empty(target):
@@ -51,13 +51,14 @@ class CARTTree:
 
         return node
 
-    def should_predict(self, data, target, depth) -> bool:
+    def should_predict(self, data: pd.DataFrame, target: pd.Series, depth: int) \
+            -> bool:
         """Return true if any of stop criterias are reached else false"""
         return depth == self.max_depth or \
                contains_one_type(target) or is_empty(data)
 
-    def _best_split(self, data, target) -> \
-            Tuple[float, str, Union[float, str], bool]:
+    def _best_split(self, data: pd.DataFrame, target: pd.Series) \
+            -> Tuple[float, str, Union[float, str], bool]:
         """Return the value, attribute name and purity of the attribute with lowest impurity
 
         Finds the val with lowest impurity for each atrribute and compares
@@ -81,7 +82,9 @@ class CARTTree:
 
         return least_impure, best_arg_name, best_val, best_is_numeric
 
-    def _make_split(self, target, attribute, val, is_numeric: bool) -> List[SplitWithInfo]:
+    def _make_split(self, target: pd.Series, attribute: pd.Series,
+                    val: Union[float, str], is_numeric: bool) \
+            -> List[SplitWithInfo]:
         """Split target into two on val"""
 
         data_splits = []
@@ -100,7 +103,8 @@ class CARTTree:
 
         return data_splits
 
-    def _best_split_value(self, target, attribute) -> Tuple[float, Union[float, str], bool]:
+    def _best_split_value(self, target: pd.Series, attribute: pd.Series) \
+            -> Tuple[float, Union[float, str], bool]:
         """Return value with the lowest gini_index"""
 
         is_numeric = attribute.dtype == 'O'
@@ -122,11 +126,11 @@ class CARTTree:
 
         return gini, best_val, is_numeric
 
-    def predict(self, data) -> Union[float, str]:
+    def predict(self, data: pd.DataFrame) -> Union[float, str]:
         """Predict target for data"""
         return self._predict(data, self.root)
 
-    def _predict(self, data, node) -> Union[float, str]:
+    def _predict(self, data: pd.DataFrame, node: Node) -> Union[float, str]:
         """Recursively follows conditions in branchs to find
         prediction, starting at node"""
 
@@ -144,7 +148,7 @@ class CARTTree:
     def print_tree(self) -> None:
         self._print_tree(self.root)
 
-    def _print_tree(self, node) -> None:
+    def _print_tree(self, node: Node) -> None:
         if isinstance(node, Leaf):
             print(f'Pred: {node.prediction}')
             return
